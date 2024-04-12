cc4199e22d743c6f5a72676b89750a2999379cb9
HasanliHM <hacaga.hasanli@kapitalbank.az>
2024-04-13 00:21:44 +0400

    created Options React Component and testing of async process of fetching images

---
+++ src/pages/entry/ScoopOption.jsx
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+
+import { Col } from "react-bootstrap";
+
+export default function ScoopOption({ name, imagePath }) {
+  return (
+    <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
+      <img
+        style={{ width: "75%" }}
+        src={`http://localhost:3030/${imagePath}`}
+        alt={`${name} scoop`}
+      />
+    </Col>
+  );
+}
+
+ScoopOption.propTypes = {
+  name: PropTypes.string,
+  imagePath: PropTypes.string,
+};
---
+++ src/pages/entry/tests/Options.test.jsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+
+import Options from "../Options";
+
+test("display image for each scoop option from server", async () => {
+  render(<Options optionType={"scoops"} />);
+
+  // find images
+  const scoopImages = await screen.findAllByRole("img", {
+    name: /scoop$/i,
+  });
+  expect(scoopImages).toHaveLength(2);
+
+  // confirm alt text of images
+  // @ts-ignore
+  const altText = scoopImages.map((element) => element.alt);
+  expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
