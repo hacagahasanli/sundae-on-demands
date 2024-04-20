34ee2d3396f65e8ecdd3eba3f3a6fa88acfec804
HasanliHM <hacaga.hasanli@kapitalbank.az>
2024-04-20 21:10:01 +0400

    Test for Grand Total

--- src/pages/entry/ui/ToppingOption.jsx
+++ src/pages/entry/ui/ToppingOption.jsx
@@ -1,8 +1,18 @@
 import PropTypes from "prop-types";
 
-import { Col } from "react-bootstrap";
+import { Col, Form } from "react-bootstrap";
+
+import { useOrderDetails } from "../../../contexts/OrderDetails.jsx";
+
+const { Group, Check } = Form;
 
 export default function ToppingOption({ name, imagePath }) {
+  const { updateItemCount } = useOrderDetails();
+
+  const handleChange = (e) => {
+    updateItemCount(name, e.target.checked ? 1 : 0, "toppings");
+  };
+
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
       <img
@@ -10,6 +20,9 @@ export default function ToppingOption({ name, imagePath }) {
         style={{ width: "75%" }}
         src={`http://localhost:3030/${imagePath}`}
       />
+      <Group controlId={`${name}-topping-checkbox`}>
+        <Check type={"checkbox"} onChange={handleChange} label={name} />
+      </Group>
     </Col>
   );
 }
